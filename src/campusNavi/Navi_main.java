a36a2061e71a357b4f1ab7411db043b94de9a36b
Seyiul <iseuli713@gmail.com>
2020-06-09 15:21:47 +0900

    School Map Service

---
+++ src/campusNavi/Navi_main.java
@@ -0,0 +1,26 @@
+package campusNavi;
+
+
+public class Navi_main {
+
+   public static void main(String[] args) throws InterruptedException {
+      
+      CampusMapGUI campus = new CampusMapGUI();
+      
+      
+      /*
+      System.out.println("확인 합니다 " + start +" 그리고 "+ destination);
+      start ="교육대학원";
+      destination ="바이오나노대학";
+      try {
+      DijkstraAlgorithm dijkstra =new DijkstraAlgorithm(start, destination);
+      }catch(Exception e) {
+         e.printStackTrace();
+         throw e;
+      }
+      finally {
+         System.out.println("finally 임");
+      }*/
+   }
+
+}
